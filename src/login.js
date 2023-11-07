80390654b6b6444bcb1b19955f92376910a4bccd
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2023-11-07 19:21:52 +0100

    Add files via upload

---
+++ src/login.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const form = document.querySelector('form');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const buttonSubmit = document.querySelector('button');
+// console.log(usernameInput, passwordInput);
+
+usernameInput.addEventListener('keyup', function () {
+  // const username = this.value;
+  if (!emptyInputs()) buttonSubmit.classList.add('btn--active');
+  else buttonSubmit.classList.remove('btn--active');
+});
+
+passwordInput.addEventListener('keyup', function () {
+  // const username = this.value;
+  if (!emptyInputs()) buttonSubmit.classList.add('btn--active');
+  else buttonSubmit.classList.remove('btn--active');
+});
+
+form.onsubmit = function (event) {
+  event.preventDefault();
+};
+
+const emptyInputs = function () {
+  return usernameInput.value == '' || passwordInput.value === '';
+};
+
+const buttonShowPassword = document.querySelector('span');
+
+buttonShowPassword.addEventListener('click', function () {
+  this.classList.toggle('show');
+  if (this.classList.contains('show'))
+    passwordInput.setAttribute('type', 'password');
+  else passwordInput.removeAttribute('type');
+});
